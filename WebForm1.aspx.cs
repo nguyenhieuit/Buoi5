767bc2120f9ec096141ea67b351b767cf26b82b7
Héo <84056145+nguyenhieuit@users.noreply.github.com>
2021-05-20 08:23:40 +0700

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            lbl_output.Text = "Trang trước";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            lbl_output.Text = "Trang chủ";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            lbl_output.Text = "Quay lại";
+        }
+    }
+}
